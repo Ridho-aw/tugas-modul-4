c4dc5c2c1118c1b3f9be9f22a8e4494920d42146
Ridho Ananta Wibowo <r9480.a2b@gmail.com>
2026-03-12 22:03:45 +0700

    menambahkan class KodeBuah

--- Tugas-Modul4/Tugas-Modul4/Program.cs
+++ Tugas-Modul4/Tugas-Modul4/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class KodeBuah
+{
+    private static string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+    enum Buah
+    {
+        Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+    }
+    public static void Main()
+    {
+        int Kode = 0;
+        string gKodeBuah;
+        foreach (Buah buah in Enum.GetValues(typeof(Buah)))
+        {
+            Console.WriteLine($"{buah.ToString()} {kodeBuah[Kode]} ");
+            Kode++;
+        }
+        Console.Write("Masukan Buah : ");
+        String ibuah = Console.ReadLine();
+        Kode = 0;
+        foreach (Buah buah in Enum.GetValues(typeof(Buah)))
+        {
+            if (ibuah.Equals(buah.ToString()))
+            {
+                gKodeBuah = getKodeBuah(Kode);
+                Console.WriteLine($"Buah {ibuah} Kode Buah {getKodeBuah}");
+                break;
+            }
+            else
+            {
+                Kode++;
+            }
+        }
+    }
+
+    private static string getKodeBuah(int kode)
+    {
+        return kodeBuah[kode];
+    }
+}
--- Tugas-Modul4/Tugas-Modul4/obj/Debug/net10.0/Tugas-Modul4.AssemblyInfo.cs
+++ Tugas-Modul4/Tugas-Modul4/obj/Debug/net10.0/Tugas-Modul4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tugas-Modul4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c64da5118c09d39e290c0acd2b5626638366d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tugas-Modul4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tugas-Modul4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
